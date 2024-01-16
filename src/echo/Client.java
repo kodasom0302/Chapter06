5d80724e20b5f0d351bdf65e13eb0c0de9617e75
dasom <dasom589@gmail.com>
2024-01-16 17:09:56 +0900

    2024-01-16 6교시

--- src/echo/Client.java
+++ src/echo/Client.java
@@ -1,6 +1,8 @@
 package echo;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -15,6 +17,15 @@ public class Client {
 		
 		System.out.println("[서버에 연결을 요청합니다]");
 		socket.connect(new InetSocketAddress("192.168.137.1", 10001));
+		
+		System.out.println("[서버에 연결 되었습니다.]");
+		
+		//메세지 전송
+		OutputStream os=socket.getOutputStream();
+		/*
+		파일인 경우
+		OutputStream out=new FileOutputStream("파일 경로");
+		*/
 		
 		socket.close();
 
