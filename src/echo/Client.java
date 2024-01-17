162be42b1bfa8603edffdae0a50f13d3177981a6
dasom <dasom589@gmail.com>
2024-01-17 10:42:38 +0900

    2024-01-17 1교시

--- src/echo/Client.java
+++ src/echo/Client.java
@@ -1,10 +1,15 @@
 package echo;
 
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -16,17 +21,43 @@ public class Client {
 		System.out.println("============================");
 		
 		System.out.println("[서버에 연결을 요청합니다]");
-		socket.connect(new InetSocketAddress("192.168.137.1", 10001));
+		socket.connect(new InetSocketAddress("192.168.0.57", 10001));
 		
 		System.out.println("[서버에 연결 되었습니다.]");
 		
-		//메세지 전송
+		//메세지 보내기용 스트림
 		OutputStream os=socket.getOutputStream();
-		/*
-		파일인 경우
-		OutputStream out=new FileOutputStream("파일 경로");
-		*/
+		OutputStreamWriter osw=new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw=new BufferedWriter(osw);
 		
+		//메세지 받기용 스트림
+		InputStream is=socket.getInputStream();
+		InputStreamReader isr=new InputStreamReader(is, "UTF-8");
+		BufferedReader br=new BufferedReader(isr);
+		
+		Scanner sc=new Scanner(System.in);
+		
+		while (true) {
+			//키보드 입력
+			String str=sc.nextLine();
+			
+			if ("/q".equals(str)) {
+				break;
+			}
+			
+			//메세지 보내기
+			bw.write(str);
+			bw.newLine();
+			bw.flush();
+			
+			//메세지 받기
+			String reMsg=br.readLine();
+			System.out.println("server ["+reMsg+"]");
+		}
+		
+		sc.close();
+		br.close();
+		bw.close();
 		socket.close();
 
 	}
--- src/echo/Server.java
+++ src/echo/Server.java
@@ -1,6 +1,12 @@
 package echo;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,8 +19,7 @@ public class Server {
 		ServerSocket serverSocket=new ServerSocket();
 		
 		//바인딩
-		InetSocketAddress aaa=new InetSocketAddress("192.168.137.1", 10001);
-		serverSocket.bind(aaa);
+		serverSocket.bind(new InetSocketAddress("192.168.0.57", 10001));
 		
 		//서버 시작
 		System.out.println("<서버 시작>");
@@ -25,6 +30,35 @@ public class Server {
 		Socket socket=serverSocket.accept();
 		System.out.println("[클라이언트가 연결 되었습니다.]");
 		
+		//메세지 받기용 스트림
+		InputStream is=socket.getInputStream();
+		InputStreamReader isr=new InputStreamReader(is, "UTF-8");
+		BufferedReader br=new BufferedReader(isr);
+		
+		//메세지 보내기용 스트림
+		OutputStream os=socket.getOutputStream();
+		OutputStreamWriter osw=new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw=new BufferedWriter(osw);
+		
+		while (true) {
+			//메세지 받기
+			String msg=br.readLine();
+			
+			if (msg==null) {
+				break;
+			}
+			
+			System.out.println("받은 메세지: "+msg);
+			
+			//메세지 보내기
+			bw.write(msg);
+			bw.newLine();
+			bw.flush();
+		}
+		
+		//닫기
+		bw.close();
+		br.close();
 		serverSocket.close();
 		socket.close();
 
